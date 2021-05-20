c3f9ccb8ffc3f11e72a4c6f4a56815b14387c0a2
Christine <69013315+christineNg1028@users.noreply.github.com>
2021-05-20 02:34:15 -0400

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/finding_favourite_times.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finding_favourite_times.dir/main.cpp.o"
+  "finding_favourite_times.exe"
+  "finding_favourite_times.pdb"
+  "libfinding_favourite_times.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finding_favourite_times.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/finding_favourite_times.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for finding_favourite_times.
+# This may be replaced when dependencies are built.
